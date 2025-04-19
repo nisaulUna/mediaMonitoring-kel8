46645b89001fae88b1915a92f9c9f3a3126155aa
febyintanola <intanfebyola@gmail.com>
2025-04-19 18:49:47 +0700

    git commit -m "feat(auth) : add forget password dan reset password"

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const { db } = require('../config');
 const bcrypt = require('bcrypt');
+const { logActivity, ACTION_TYPES } = require('../activityLogger');
 
 // Get all users
 exports.getUsers = async (req, res) => {
@@ -46,28 +47,42 @@ exports.updateUser = async (req, res) => {
 
 // Delete user
 exports.deleteUser = async (req, res) => {
-  const userId = req.user.id
-  try {
+  const userId = req.user.id;
+  const username = req.user.username; 
 
+  try {
+    // Ambil proyek aktif milik user
     const [projects] = await db.query(
       'SELECT project_name FROM projects WHERE id_user = ? AND is_active = 1',
       [userId]
-    )
+    );
 
+    // Nonaktifkan proyek
     await db.query(
-      'UPDATE projects SET is_active = 0, deletedAt = NOW() WHERE id_user = ?',
+      'UPDATE projects SET is_active = 0 WHERE id_user = ?',
       [userId]
-    )
+    );
 
+    // Simpan info ke Redis
     for (const project of projects) {
-      const project_name = project.project_name
+      const project_name = project.project_name;
       await redis.set(
         `delete_project:${project_name}:${userId}`,
-        JSON.stringify({ project_name, userId: userId })
-      )
+        JSON.stringify({ project_name, userId })
+      );
     }
 
+    // Log aktivitas penghapusan akun
+    const ipAddress = req.ip || req.connection.remoteAddress;
+    await logActivity(userId, ACTION_TYPES.DELETE_ACCOUNT, req, {
+      username,
+      ip: ipAddress,
+    });
+
+    // Hapus user dari database
+    await db.query('DELETE FROM log_activities WHERE id_user = ?', [userId]);
     await db.query('DELETE FROM users WHERE id = ?', [userId]);
+
     res.status(200).json({ message: 'User deleted' });
   } catch (err) {
     res.status(500).json({ error: err.message });
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -6,5 +6,7 @@ const authMiddleware = require('../../middlewares/authMiddleware');
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.post('/logout', authMiddleware , authController.logout);
+router.post('/forgot-password', authController.forgotPassword);
+router.post('/reset-password', authController.resetPassword);
 
 module.exports = router;
